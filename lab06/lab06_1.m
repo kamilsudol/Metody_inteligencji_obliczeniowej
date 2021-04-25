722112c19b099a93e8c36d76293080788751e229
Kamil Sudoł <61806373+kamilsudol@users.noreply.github.com>
2021-04-26 00:25:24 +0200

    lab07 update

--- lab06/lab06_1.m
+++ lab06/lab06_1.m
@@ -1,5 +1,6 @@
 clear; clc;
 
+my_fuzzy = readfis('lab06');
 raw_data = readtable('lab7_data.csv');
 
 train_amount = height(raw_data)*0.8;
@@ -21,8 +22,14 @@ fis = genfis(train(:,1:3), train(:,4:4), opt);
 ymodel = evalfis(fis,train(:,1:3));
 ymodel_test = evalfis(fis,test(:,1:3));
 
+ymodel_myfuzzy = evalfis(my_fuzzy,train(:,1:3));
+ymodel_test_myfuzzy = evalfis(my_fuzzy,test(:,1:3));
+
 error_train = immse(ymodel, train(:,4:4));
 error_test = immse(ymodel_test, test(:,4:4));
+
+error_train_myfuzzy = immse(ymodel_myfuzzy, train(:,4:4));
+error_test_myfuzzy = immse(ymodel_test_myfuzzy, test(:,4:4));
 % fuzzy
 % showrule(fis_train)
 % fis_test = genfis(test(:,1:3), test(:,4:4), opt);
